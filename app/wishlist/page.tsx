5eda919d75a2feaf36bb0dbd9a57cc7dc506fb31
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-10-20 10:52:44 +0530

    update layout.tsx 🔵 other: add missing parameters in the wishlist page

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -4,30 +4,31 @@ import { exploreProducts, wishlistProducts } from "@/lib";
 
 export default function page() {
   return (
-    <div className="w-full py-10 flex flex-col gap-y-10 items-center justify-center px-10">
-      <div className="w-full flex items-center justify-between">
-        <h6 className="font-normal text-black/90 ">WishList (4) </h6>
-        <button className="px-3 py-2 rounded-[4px] border border-black/30 flex items-center justify-center hover:bg-secondaryLightGraryColor hover:text-black transition-all duration-200 cursor-pointer">
+    <div className="flex w-full flex-col items-center justify-center gap-y-10 px-10 py-10">
+      <div className="flex w-full items-center justify-between">
+        <h6 className="font-normal text-black/90">WishList (4) </h6>
+        <button className="flex cursor-pointer items-center justify-center rounded-[4px] border border-black/30 px-3 py-2 transition-all duration-200 hover:bg-secondaryLightGraryColor hover:text-black">
           Move All To Bag
         </button>
       </div>
-      <div className="w-full grid grid-cols-2 lg:grid-cols-4 gap-x-3">
+      <div className="grid w-full grid-cols-2 gap-x-3 lg:grid-cols-4">
         {wishlistProducts.map((item, index) => {
-          return <ProductItem item={item} key={index} />;
+          return <ProductItem categoryName="" item={item} key={index} />;
         })}
       </div>
 
       {/* recommendations */}
-      <div className="flex w-full gap-y-3 justify-between mt-10">
+      <div className="mt-10 flex w-full justify-between gap-y-3">
         <PrimaryTitleGenerator title={"Just For you"} />
-        <button className="border w-[10rem] px-3 p-1 border-black/30 flex items-center justify-center hover:bg-secondaryLightGraryColor duration-300 transition-all cursor-pointer">
+        <button className="flex w-[10rem] cursor-pointer items-center justify-center border border-black/30 p-1 px-3 transition-all duration-300 hover:bg-secondaryLightGraryColor">
           See All
         </button>
       </div>
 
-      <div className="grid md:grid-cols-3 place-items-center px-10 md:px-3 lg:grid-cols-4 py-5 gap-x-3 gap-y-3 w-full">
+      <div className="grid w-full place-items-center gap-x-3 gap-y-3 px-10 py-5 md:grid-cols-3 md:px-3 lg:grid-cols-4">
         {exploreProducts.map((item, index) => {
-          if (index > 3) return <ProductItem key={index} item={item} />;
+          if (index > 3)
+            return <ProductItem categoryName="" key={index} item={item} />;
         })}
       </div>
     </div>
